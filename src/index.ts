b37ed7a8963ff1e9af43b89c7007aa0a57a2eed8
power <trusionp@gmail.com>
2020-03-10 22:55:37 -0600

    auto restart

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import "reflect-metadata";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import express_user_ip from "express-ip";
+import cluster from "cluster";
+
 import { buildFederatedSchema } from "./helpers/buildFederatedSchema";
 //?  decorators metadata
 
@@ -14,41 +16,49 @@ import {
   resolveCommentaryReference
 } from "./schema/CommentarySchema";
 const PORT: string = process.env.PORT || "3000";
+if (cluster.isMaster) {
+  cluster.fork();
 
-(async () => {
-  try {
-    // Initialize the app
-    const app = express();
-    app.use(express_user_ip().getIpInfoMiddleware); //* get the user location data
+  cluster.on("exit", function(worker, code, signal) {
+    cluster.fork();
+  });
+}
+if (cluster.isWorker) {
+  (async () => {
+    try {
+      // Initialize the app
+      const app = express();
+      app.use(express_user_ip().getIpInfoMiddleware); //* get the user location data
 
-    const server = new ApolloServer({
-      schema: await buildFederatedSchema(
-        {
-          resolvers: [CommentaryResolver],
-          orphanedTypes: [Commentary, Challenge, User]
+      const server = new ApolloServer({
+        schema: await buildFederatedSchema(
+          {
+            resolvers: [CommentaryResolver],
+            orphanedTypes: [Commentary, Challenge, User]
+          },
+          {
+            Commentary: { __resolveReference: resolveCommentaryReference }
+          }
+        ),
+        context: req => req,
+        formatError: err => {
+          return err;
         },
-        {
-          Commentary: { __resolveReference: resolveCommentaryReference }
-        }
-      ),
-      context: req => req,
-      formatError: err => {
-        return err;
-      },
-      tracing: false,
-      playground: true
-    });
-    // The GraphQL endpoint
+        tracing: false,
+        playground: true
+      });
+      // The GraphQL endpoint
 
-    server.applyMiddleware({ app, path: "/graphql" });
+      server.applyMiddleware({ app, path: "/graphql" });
 
-    // Start the server
-    await connectDB();
+      // Start the server
+      await connectDB();
 
-    app.listen(PORT, () => {
-      console.log(`Go to http://localhost:${PORT}/graphiql to run queries!`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-})();
+      app.listen(PORT, () => {
+        console.log(`Go to http://localhost:${PORT}/graphiql to run queries!`);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  })();
+}
